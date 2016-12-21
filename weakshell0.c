b84c245a04102c9cb422e750821fbbb3791de8f2
Daisuke TAKAGO <takago@neptune.kanazawa-it.ac.jp>
2016-12-22 00:09:27 +0900

    3rd Commit

--- weakshell0.c
+++ weakshell0.c
@@ -22,7 +22,7 @@
 #define MAX_ARGS 10
 
 // 10個まで引数をとることができる．
-char *_argv[MAX_ARGS]; // ポインタの配列
+char *_argv[MAX_ARGS];			// ポインタの配列
 
 
 /* 入力行をトークンに区切り，各コマンドライン引数を_argv[]に格納する 
@@ -30,57 +30,57 @@ char *_argv[MAX_ARGS]; // ポインタの配列
  */
 void parseLine(char *str)
 {
-	int n=0;	
-	
+	int n = 0;
+
 	char buf[BUFSZ];
-	char *p=buf;
+	char *p = buf;
 	char *cmd;
-    for(n=0;n<MAX_ARGS;n++){
-        if(_argv[n]!=NULL)
-            free(_argv[n]);		
-        _argv[n]=NULL;
-    }    
+	for (n = 0; n < MAX_ARGS; n++) {
+		if (_argv[n] != NULL)
+			free(_argv[n]);
+		_argv[n] = NULL;
+	}
 	strcpy(buf, str);
-    n=0;    
-	while((cmd=strtok_r(p, " \n", &p))!=NULL){ /* まずスペース部分で分離 */
+	n = 0;
+	while ((cmd = strtok_r(p, " \n", &p)) != NULL) {	/* まずスペース部分で分離 */
 		/* ポインタの配列に格納 */
-        _argv[n]=malloc(BUFSZ);
-        strcpy(_argv[n],cmd);
-        n++;
+		_argv[n] = malloc(BUFSZ);
+		strcpy(_argv[n], cmd);
+		n++;
 	}
 }
 
 void viewToken()
 {
-	fprintf(stderr,"\x1b[31m------- COMMAND LINE -----------\n");	
+	fprintf(stderr, "\x1b[31m------- COMMAND LINE -----------\n");
 	int n;
-    n=0;		
-    while(_argv[n]!=NULL){			
-        fprintf(stderr,"\t%s\n",_argv[n]);
-        n++;			
-    }
-	fprintf(stderr,"--------------------------------\n\x1b[39m");	
+	n = 0;
+	while (_argv[n] != NULL) {
+		fprintf(stderr, "\t%s\n", _argv[n]);
+		n++;
+	}
+	fprintf(stderr, "--------------------------------\n\x1b[39m");
 }
 
 void execLine()
-{	
+{
 	// 子プロセスの生成
-    if(fork()==0){ 
-        execvp(_argv[0], _argv);
-        exit(1);			
-    }
-    wait(NULL);
+	if (fork() == 0) {
+		execvp(_argv[0], _argv);
+		exit(1);
+	}
+	wait(NULL);
 }
 
 int main()
 {
 	char buf[BUFSZ];
-	printf("wsh0>> ");	
-	while(fgets(buf,BUFSZ,stdin)!=NULL){
+	printf("wsh0>> ");
+	while (fgets(buf, BUFSZ, stdin) != NULL) {
 		parseLine(buf);
 		viewToken();
 		execLine();
-		printf("wsh0>> ");	
+		printf("wsh0>> ");
 	}
-	return 0;	
+	return 0;
 }
--- weakshell1.c
+++ weakshell1.c
@@ -20,14 +20,14 @@
 #include <fcntl.h>
 
 #define BUFSZ 256
-#define MAX_ARGS 10 // 1コマンドあたりの最大の引数の個数
-#define MAX_CMDS 5  // 起動する最大のコマンド数
+#define MAX_ARGS 10	// 1コマンドあたりの最大の引数の個数
+#define MAX_CMDS 5	// 起動する最大のコマンド数
 
-int _M;  // 1行に含まれるコマンド数
+int _M;							// 1行に含まれるコマンド数
 
 // 10個まで引数をとることができる．クォートはだめ
-char *_argv[MAX_CMDS][MAX_ARGS]; // ポインタの配列
-    // parse()後はcmd_argv[コマンド番号][引数番号]で取り出せる
+char *_argv[MAX_CMDS][MAX_ARGS];	// ポインタの配列
+	// parse()後はcmd_argv[コマンド番号][引数番号]で取り出せる
 
 
 /* 入力行をトークンに区切り，各コマンドライン引数を_argv[][]に格納する 
@@ -35,37 +35,39 @@ char *_argv[MAX_CMDS][MAX_ARGS]; // ポインタの配列
  */
 void parseLine(char *str)
 {
-	int n=0;	
-	
+	int n = 0;
+
 	char buf[BUFSZ];
-	char *line=buf;
+	char *line = buf;
 	char *p;
 	char *cmd;
 
-	_M=0;
-    int m;    
-    for(n=0;n<MAX_ARGS;n++){
-        for(m=0;m<MAX_CMDS;m++){
-            if(_argv[m][n]!=NULL)
-                free(_argv[m][n]);		
-            _argv[m][n]=NULL;
-        }
-    }    
-    
-	strcpy(buf, str);	
-	while((p=strtok_r(line, "|", &line))!=NULL){ /* まずパイプ部分で分離 */
-		
-        /* 先頭の空白文字を読み飛ばす */
-        while(isspace(*p)) p++;
+	_M = 0;
+	int m;
+	for (n = 0; n < MAX_ARGS; n++) {
+		for (m = 0; m < MAX_CMDS; m++) {
+			if (_argv[m][n] != NULL)
+				free(_argv[m][n]);
+			_argv[m][n] = NULL;
+		}
+	}
 
-        /* 末尾の空白文字を消去 */
-		while(isspace(p[strlen(p)-1])) p[strlen(p)-1]='\0';
-		
+	strcpy(buf, str);
+	while ((p = strtok_r(line, "|", &line)) != NULL) {	/* まずパイプ部分で分離 */
+
+		/* 先頭の空白文字を読み飛ばす */
+		while (isspace(*p))
+			p++;
+
+		/* 末尾の空白文字を消去 */
+		while (isspace(p[strlen(p) - 1]))
+			p[strlen(p) - 1] = '\0';
+
 		/* ポインタの配列に格納 */
-		n=0;		
-		while((cmd=strtok_r(p, " ", &p))!=NULL) {
-            _argv[_M][n]=malloc(BUFSZ);
-			strcpy(_argv[_M][n],cmd);
+		n = 0;
+		while ((cmd = strtok_r(p, " ", &p)) != NULL) {
+			_argv[_M][n] = malloc(BUFSZ);
+			strcpy(_argv[_M][n], cmd);
 			n++;
 		}
 		_M++;
@@ -74,67 +76,67 @@ void parseLine(char *str)
 
 void viewToken()
 {
-	fprintf(stderr,"\x1b[31m------- COMMAND LINE -----------\n");	
-	int m,n;
-	for(m=0;m<_M;m++){
-		n=0;		
-		while(_argv[m][n]!=NULL){			
-			fprintf(stderr,"\t%s\n",_argv[m][n]);
-			n++;			
+	fprintf(stderr, "\x1b[31m------- COMMAND LINE -----------\n");
+	int m, n;
+	for (m = 0; m < _M; m++) {
+		n = 0;
+		while (_argv[m][n] != NULL) {
+			fprintf(stderr, "\t%s\n", _argv[m][n]);
+			n++;
 		}
-		if(m<_M)
-			fprintf(stderr,"PIPE:\n");		
+		if (m < _M)
+			fprintf(stderr, "PIPE:\n");
 	}
-	fprintf(stderr,"--------------------------------\n\x1b[39m");	
+	fprintf(stderr, "--------------------------------\n\x1b[39m");
 }
 
 void execLine()
-{	
-	int fd[MAX_CMDS+1][2]; // dup2()で使う
-	int m=0;
+{
+	int fd[MAX_CMDS + 1][2];	// dup2()で使う
+	int m = 0;
 	int k;
 
 	// 使用するファイルディスクリプタ
-	fd[0][0]=0; // 最初の入力は標準入力
-	for(k=1;k<_M;k++){		
-		pipe(fd[k]); // パイプはfork()する前に生成
+	fd[0][0] = 0;	// 最初の入力は標準入力
+	for (k = 1; k < _M; k++) {
+		pipe(fd[k]);	// パイプはfork()する前に生成
 	}
-	fd[_M][1]=1; // 最後の出力は標準出力
+	fd[_M][1] = 1;	// 最後の出力は標準出力
 
 	// 子プロセスの生成
-	for(m=0;m<_M;m++){
-		if(fork()==0){             
-			dup2(fd[m][0],0);   // 標準入力の設定
-			dup2(fd[m+1][1],1); // 標準出力の設定
-			for(k=1;k<_M;k++){	 // 使わないFD（パイプのFD）を閉じる	
+	for (m = 0; m < _M; m++) {
+		if (fork() == 0) {
+			dup2(fd[m][0], 0);	// 標準入力の設定
+			dup2(fd[m + 1][1], 1);	// 標準出力の設定
+			for (k = 1; k < _M; k++) {	// 使わないFD（パイプのFD）を閉じる  
 				close(fd[k][0]);
 				close(fd[k][1]);
 			}
 			execvp(_argv[m][0], _argv[m]);
-			exit(1);			
+			exit(1);
 		}
 	}
 	// 使わないFD（パイプのFD）を閉じる
-	for(k=1;k<_M;k++){ 		
+	for (k = 1; k < _M; k++) {
 		close(fd[k][0]);
 		close(fd[k][1]);
 	}
 	// 全子プロセスの終了を待つ
-	for(m=0;m<_M;m++){
+	for (m = 0; m < _M; m++) {
 		wait(NULL);
-	}		
+	}
 
 }
 
 int main()
 {
 	char buf[BUFSZ];
-	printf("wsh1>> ");	
-	while(fgets(buf,BUFSZ,stdin)!=NULL){
+	printf("wsh1>> ");
+	while (fgets(buf, BUFSZ, stdin) != NULL) {
 		parseLine(buf);
 		viewToken();
 		execLine();
-		printf("wsh1>> ");	
+		printf("wsh1>> ");
 	}
-	return 0;	
+	return 0;
 }
